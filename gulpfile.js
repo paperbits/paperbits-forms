1f15c59c3b1b8c7a8ea8af92d12c2a7b175a2904
azaslonov <zaslonov.alexander@gmail.com>
2018-06-05 22:37:39 -0700

    minor fixes

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,11 @@ const typescript = require("typescript");
 const typescriptCompiler = require("gulp-typescript");
 const del = require("del");
 const merge = require("merge2");
+const runSeq = require("run-sequence");
 
-gulp.task("build-clean", (done) => {
-    return del(["dist/lib/**"], done);
+gulp.task("build-npm", ["build-npm-ts"], () => {
+    return gulp.src(["!./src/themes/**/*.*", "./src/**/*.+(html|scss|png|woff|woff2|eot|ttf|svg)"])
+        .pipe(gulp.dest("./dist/lib"));
 });
 
 gulp.task("build-npm-ts", ["build-clean"], (callback) => {
@@ -23,7 +25,11 @@ gulp.task("build-npm-ts", ["build-clean"], (callback) => {
     ]);
 });
 
-gulp.task("build", ["build-npm-ts"]);
+gulp.task("build-clean", (done) => {
+    return del(["dist/lib/**"], done);
+});
+
+gulp.task("build", (done) => runSeq("build-clean", "build-npm-ts", done));
 
 gulp.task("default", ["build"]);
 
